b46ff7285fd22e5ae7df6f570df40440d442281e
Deni Kiseljak <deni.kiseljak@gmail.com>
2022-01-29 17:28:19 +0100

    Add project files.

---
+++ CSH projekt/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSH_projekt
+{
+    public partial class Form1 : Form
+    {
+
+        private void Deck()
+        {
+            private string[] suite = ["Hearts", "Spades", "Diamonds", "Clubs"];
+            private string[] value = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"];
+            private string[] signs = ["\uU + 2660"]
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Deck();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
